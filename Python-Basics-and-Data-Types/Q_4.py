adc3e4d312febe62cf004ac8495e0c249b642db4
Rohan Bangar <bangarrohan@2005gmail.com>
2026-06-26 12:03:08 +0530

    Solve Convert Celsius to Fahrenheit

---
+++ Python-Basics-and-Data-Types/Q_4.py
@@ -0,0 +1,12 @@
+# Convert Celsius to Fahrenheit.
+# Formula:
+# F = (C × 9/5) + 32
+
+
+def celsius_to_fahrenheit(celsius):
+    """
+        Convert temperature from Celsius to Fahrenheit.
+    """
+    return (celsius * 9/5) + 32
+
+print(celsius_to_fahrenheit(-40))
