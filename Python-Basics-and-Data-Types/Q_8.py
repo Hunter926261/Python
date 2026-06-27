77203a021c8485aa1d11b4060cd00e67ce8fec50
Rohan Bangar <bangarrohan@2005gmail.com>
2026-06-27 19:10:47 +0530

    Solved: Last digit problem

---
+++ Python-Basics-and-Data-Types/Q_8.py
@@ -0,0 +1,20 @@
+# Find the last digit of a number
+
+
+def last_digit(num: int) -> int:
+    """
+    Return the last digit of number
+    Args:
+        num(int): Input integer
+    Returns: 
+        int: last digit of integer
+    Raises: 
+        TypeError: If num is not integer
+    """
+    
+    if not isinstance(num,int):
+        raise TypeError("Input must be the integer")
+    
+    return abs(num)%10
+
+print(last_digit(9644))
