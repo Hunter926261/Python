2024cf499bdb1eb04fd3d30aa266bb02d135c55c
Rohan Bangar <bangarrohan@2005gmail.com>
2026-06-27 19:34:15 +0530

    Solved: Counting number of digits in integer

---
+++ Python-Basics-and-Data-Types/Q_9.py
@@ -0,0 +1,26 @@
+# Count the number of digits in an integer
+
+def count_digit(num: int) -> int:
+    """
+    Return count of digit in integer
+    Args:
+        num(int): Enter integer
+    Returns: 
+        (int): count of digits
+    """
+
+    if not isinstance(num,int):
+        raise TypeError("Input must be the integer")
+
+    num = abs(num)
+    count = 0
+
+    if num == 0:
+        return 1
+    while num>0:
+        count += 1
+        num //= 10
+
+    return count
+
+print(count_digit(-123))
